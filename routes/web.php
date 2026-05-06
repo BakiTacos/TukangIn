8795097c7a41eedff6015b788d8aa652e605b25f
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2026-05-06 15:59:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
