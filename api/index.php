b7a91b7c50daa9eb1b2dc2555eb4f15f3b97e14d
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2026-05-06 16:10:05 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,17 +1,16 @@
 <?php
-// 1. Aktifkan Error Reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// Pastikan path ini benar-benar mengarah ke folder vendor
+require __DIR__ . '/../vendor/autoload.php';
 
-// 2. Paksa Laravel menulis ke folder /tmp (Sangat Penting di Vercel!)
-// Karena folder storage asli di Vercel bersifat Read-Only
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-putenv('SESSION_DRIVER=cookie');
-putenv('LOG_CHANNEL=stderr');
+// Pastikan path ini mengarah ke file bootstrap app
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Buat folder temporary jika belum ada
-if (!is_dir('/tmp/storage/framework/views')) {
-    mkdir('/tmp/storage/framework/views', 0755, true);
-}
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-require __DIR__ . '/../public/index.php';
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
