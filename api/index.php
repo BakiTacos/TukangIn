ae9dae8df9130fbd09517f5cf28fef98b84aefb1
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2026-05-06 16:07:01 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,17 @@
 <?php
-
-// TIGA BARIS INI WAJIB UNTUK DEBUGGING DI VERCEL
+// 1. Aktifkan Error Reporting
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Bawaan Vercel-PHP (Jangan dihapus)
+// 2. Paksa Laravel menulis ke folder /tmp (Sangat Penting di Vercel!)
+// Karena folder storage asli di Vercel bersifat Read-Only
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
+// Buat folder temporary jika belum ada
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
