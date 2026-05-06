496a98e6a2f7eeb331d308206929bffa8011f546
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2026-05-06 16:00:57 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,21 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
+// HANYA ADA SATU RUTE UNTUK '/'
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
- Route::get('/tukang', [TukangController::class, 'index'])->name('tukang.index');
+Route::get('/tukang', [TukangController::class, 'index'])->name('tukang.index');
 
- Route::get('/layanan', [ServiceController::class, 'index'])->name('layanan.index');
-
- Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/layanan', [ServiceController::class, 'index'])->name('layanan.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 require __DIR__.'/auth.php';
